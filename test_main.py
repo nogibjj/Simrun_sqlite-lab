2aab72afae3031e9d6ac0564ec93e37f11ee1f63
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-01 18:32:36 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,8 +3,14 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.query import query
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_query():
+    assert query("SELECT "Facility ID" FROM PatientDB where "HCAHPS Answer Description" = "Nurse communication - star rating" ") == [('Facility ID',), ('340001',)]
+    
+    pass
+def test_extract():
+    pass
+def test_transform_load():
+    pass
