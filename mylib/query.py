a15073d0c3180219baad7b42846048addb68179f
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-01 21:27:54 +0000

    query

--- mylib/query.py
+++ mylib/query.py
@@ -7,17 +7,10 @@ def query(the_query):
     """Query the database for all rows of the PatientDB table"""
     conn = sqlite3.connect("PatientDB.db")
     cursor = conn.cursor()
-    sql_query = ''' 
-    SELECT "Effectiveness of care national comparison", 
-       "Readmission national comparison", 
-       "Facility Name", 
-       "State" FROM patientDB
-       WHERE "State" = 'NC';
-       '''
-    cursor.execute(sql_query)
-    for row in cursor.fetchall():
-        print(row)
+    cursor.execute(the_query)
+    rows = cursor.fetchall()
+    print(rows)
     conn.close()
-    return 
+    return rows
 
 
