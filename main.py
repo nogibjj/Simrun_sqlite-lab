6df339b66e5f47797e1d04cd609217f54366b2e1
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-01 18:05:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 ETL-Query script
 """
-
+import fire
 from mylib.extract import extract
 from mylib.transform_load import load
 from mylib.query import query
@@ -16,4 +16,7 @@ load()
 
 # Query
 print("Querying data...")
-query()
+query(the_query)
+
+if __name__ == "__main__":
+  fire.Fire(main)
