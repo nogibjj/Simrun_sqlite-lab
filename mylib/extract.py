37337e7698b17ab4da48ee335475ea7bc888596a
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-01 18:03:06 -0400

    Update extract.py

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,8 +5,8 @@ Patient Satisfaction dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://github.com/nogibjj/Simrun_sqlite-lab/raw/main/data/NC_data.csv", 
+            file_path="data/NC_data.csv"):
     """"Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, 'wb') as f:
