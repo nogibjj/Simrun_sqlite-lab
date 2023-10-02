ec9d47ef638d7cc1dcb26151579b5c4703068896
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-02 20:49:28 +0000

    database

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -10,8 +10,9 @@ from pprint import pprint
 def load(the_query):
     dataset = "data/Jeopardy.csv"
     df = pd.read_csv(dataset)
-    connect = sqlite3.connect("JeopardyDB.db")
-
+    database_path = "/workspaces/Simrun_sqlite-lab/JeopardyDB.db"
+    connect = sqlite3.connect(database_path)
+   
     df.to_sql("table1", connect, if_exists="replace", index=False)
     cursor = connect.cursor()
     cursor.execute(the_query)
