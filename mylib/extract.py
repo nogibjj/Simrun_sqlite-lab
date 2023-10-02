5173fea68c8e249e9ea911f3715434d0465a1700
unknown <simrunsharma17@gmail.com>
2023-10-01 20:01:17 -0400

    additions

--- mylib/extract.py
+++ mylib/extract.py
@@ -5,13 +5,13 @@ Patient Satisfaction dataset
 """
 import requests
 
-def extract(url="https://github.com/nogibjj/Simrun_sqlite-lab/raw/main/data/NC_data.csv", 
-            file_path="data/NC_data.csv"):
-    """"Extract a url to a file path"""
+
+def extract(
+    url="https://raw.githubusercontent.com/nogibjj/Simrun_sqlite-lab/main/data/NC_data.csv",
+    file_path="data/NC_data.csv",
+):
+    """ "Extract a url to a file path"""
     with requests.get(url) as r:
-        with open(file_path, 'wb') as f:
+        with open(file_path, "wb") as f:
             f.write(r.content)
     return file_path
-
-
-
--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -7,41 +7,29 @@ import sqlite3
 import csv
 import os
 
-#load the csv file and insert into a new sqlite3 database
-def load(dataset="https://github.com/nogibjj/Simrun_sqlite-lab/raw/main/data/NC_data.csv"):
-    """"Transforms and Loads data into the local SQLite3 database"""
 
-    #prints the full working directory and path
+# load the csv file and insert into a new sqlite3 database
+def load(dataset="data/NC_data.csv"):
+    """ "Transforms and Loads data into the local SQLite3 database"""
+
+    # prints the full working directory and path
     print(os.getcwd())
-    payload = csv.reader(open(dataset, newline=''), delimiter=',')
+    payload = csv.reader(open(dataset, newline=""), delimiter=",")
     print(payload)
     file = "PatientDB.db"
     conn = sqlite3.connect(file)
     c = conn.cursor()
     c.execute("DROP TABLE IF EXISTS PatientDB")
     c.execute(
-    """
+        """
     CREATE TABLE PatientDB 
-    (Facility ID,Facility Name,Address,City,State,ZIP Code,County Name,Phone Number,
-    HCAHPS Measure ID,HCAHPS Question,HCAHPS Answer Description,Patient Survey Star Rating,
-    Patient Survey Star Rating Footnote,HCAHPS Answer Percent,HCAHPS Answer Percent Footnote,
-    HCAHPS Linear Mean Value,Number of Completed Surveys,Number of Completed Surveys Footnote,
-    Survey Response Rate Percent,Survey Response Rate Percent Footnote,Start Date,End Date,Year,
-    Hospital Type,Hospital Ownership,Emergency Services,Meets criteria for promoting interoperability of EHRs,
-    Hospital overall rating,Hospital overall rating footnote,Mortality national comparison,
-    Mortality national comparison footnote,
-    Safety of care national comparison,Safety of care national comparison footnote,
-    Readmission national comparison,Readmission national comparison footnote,Patient experience national comparison,
-    Patient experience national comparison footnote,Effectiveness of care national comparison,
-    Effectiveness of care national comparison footnote,Timeliness of care national comparison,
-    Timeliness of care national comparison footnote,
-    Efficient use of medical imaging national comparison,
-    Efficient use of medical imaging national comparison footnote)"""
-            )
-    #insert
-    c.executemany("INSERT INTO PatientDB VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", payload)
+    ("Facility ID","State", "HCAHPS Answer Description"
+    "Readmission national comparison","Effectiveness of care national comparison",
+    "Efficient use of medical imaging national comparison")"""
+    )
+    # insert
+    c.executemany("INSERT INTO PatientDB VALUES (?, ?, ?, ?, ?)", payload)
 
     conn.commit()
     conn.close()
     return "PatientDB.db"
-
