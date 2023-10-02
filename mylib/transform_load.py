d50a29c17e426dff2adf0fa2dd045c11730e0d9e
simrunsharma <141798228+simrunsharma@users.noreply.github.com>
2023-10-02 01:21:50 -0400

    Update transform_load.py

--- mylib/transform_load.py
+++ mylib/transform_load.py
@@ -1,7 +1,7 @@
 """
 Transforms and Loads data into the local SQLite3 database
 Example:
-,general name,count_products,ingred_FPro,avg_FPro_products,avg_distance_root,ingred_normalization_term,semantic_tree_name,semantic_tree_node
+"Show Number", "Air Date", "Round", "Category", "Value", "Question", "Answer"
 """
 import chardet
 import sqlite3
